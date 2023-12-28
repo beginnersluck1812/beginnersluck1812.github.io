3ee7427b763f3a7b4297296fabcf478ac9e34125
beginnersluck1812 <122133897+beginnersluck1812@users.noreply.github.com>
2023-12-28 10:09:23 -0800

    intro done

--- base/load_html.js
+++ base/load_html.js
@@ -39,8 +39,8 @@
 const sideLinksHTML = `
 <h4 class="tutorial-links just-content">Tutorial Roadmap</h4>
 <ul class="tutorial-links just-content link-houser">
-    <li class="link-houser" ><a href="#code">1. Code</a></li>
-    <li class="link-houser" ><a href="#about">2. About</a></li>
+    <li class="link-houser" ><a href="#code">Introduction</a></li>
+    <li class="link-houser" ><a href="#about">Overview</a></li>
     <li class="link-houser" ><a href="#resources">3. Resources</a></li>
 </ul>
 `;
@@ -80,10 +80,33 @@ function loadHeader() {
 }
 
 
+var sideLinkToBold;
+
+function changeSideLinkToBold(sideLinkName) {
+  sideLinkToBold = sideLinkName;
+}
+
+//bolds the tutorial link on the side of the screen
+//so if 'Introduction' is passed in, it will bold 'Introduction' link on the side
+function boldTutorialLink(linkName) {
+
+  // Get all elements with the class 'link-houser'
+  var linkElements = document.getElementsByClassName('link-houser');
+  console.log(linkElements);
+
+    // Iterate through each element and find the one with the name 'linkName'
+    for (var i = 0; i < linkElements.length; i++) {
+      if (linkElements[i].innerText == linkName) {
+        linkElements[i].style.fontWeight = 'bold';
+        return;
+      }
+    }
+}
 
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Script is running!");
     loadSideLinks();
     loadHeader();
+    boldTutorialLink(sideLinkToBold);
 });
