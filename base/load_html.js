2b4e09538d1593325acc69508e258715ef46957a
beginnersluck1812 <122133897+beginnersluck1812@users.noreply.github.com>
2024-01-29 20:52:20 -0800

    reworded/formatted

--- base/load_html.js
+++ base/load_html.js
@@ -1,49 +1,15 @@
-{/* <body>
-  <div id="tutorial-links">
-
-    <h4 class="tutorial-links just-content">Tutorial Roadmap</h4>
-
-    <ul class="tutorial-links just-content link-houser">
-      <li class="link-houser" ><a href="#code">1. Code</a></li>
-      <li class="link-houser" ><a href="#about">2. About</a></li>
-      <li class="link-houser" ><a href="#resources">3.Resources</a></li>
-    </ul>
-
-  </div>
-  
-  
-  <div id="main-part-of-page" class="main-page">
-    <header class="main-page">
-      
-      <h1 class="main page">Othello AI Tutorial</h1>
-    
-
-      <ul class="main-page just-content link-houser">
-        <li class="link-houser" ><a href="#code">Code</a></li>
-        <li class="link-houser" ><a  href="#about">About</a></li>
-        <li class="link-houser" ><a href="#resources">Resources</a></li>
-      </ul>
-
-    </header>
-    
-    <div class="main-page" id="text-content">
-      <h2>Overview</h2>
-    </div>
-  </div>
-  
-  
-    
-</body> */}
 
 
+//all this html is going to be the same for each page, so we are going to dynamically add it
 const sideLinksHTML = `
 <h4 class="tutorial-links just-content">Tutorial Roadmap</h4>
 <ul class="tutorial-links just-content link-houser">
     <li class="link-houser" ><a href="index.html">Introduction</a></li>
     <li class="link-houser" ><a href="overview.html">Overview</a></li>
-    <li class="link-houser" ><a href="#resources">3. Resources</a></li>
+    <li class="nested-link-houser" ><a href="resources">Finding Positions</a></li>
+    <li class="link-houser" ><a href="resources">Resources</a></li>
 </ul>
-`;
+`
 
 const headerHTML = `
     <h1 class="main page">Othello AI Tutorial</h1>
@@ -54,6 +20,7 @@ const headerHTML = `
     </ul>
 `;
 
+ 
 
 
 function addHTMLToElement(elementName, HTML) {
