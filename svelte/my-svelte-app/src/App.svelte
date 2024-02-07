8168806d720497e9331ff1034f43317be84f3206
beginnersluck1812 <122133897+beginnersluck1812@users.noreply.github.com>
2024-02-07 09:02:34 -0800

    setting up header and css styles

--- svelte/my-svelte-app/src/App.svelte
+++ svelte/my-svelte-app/src/App.svelte
@@ -1,47 +1,41 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<header>
+  <h1>Othello AI Tutorial</h1>
+<ul class="main-page just-content link-houser">
+    <li class="link-houser" ><a href="#code">Code</a></li>
+    <li class="link-houser" ><a  href="#about">About</a></li>
+    <li class="link-houser" ><a href="#resources">Resources</a></li>
+</ul>
+</header>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+ header {
+    margin-left: 20%;
+    width: 100%;
+    background-color: rgb(214, 121, 121);
+    border-bottom: 2px solid black;
+    height: 10%;
+    margin-bottom: 15px;
+    display: flex;
+    align-items: center;
+ }
+
+ h1 {
+    text-align: left;
+    white-space: nowrap;
+ }
+
+ ul {
+  display: flex;
+  text-align: center;
+  padding: 10px;
+  list-style-type: none;
+  margin-left: auto;
+ }
+
+
 </style>
